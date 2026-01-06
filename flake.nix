34dc2ce9d9dd65ace6b351479950291b7c4e3bd4
Alec Featherston <alecfeatherston@gmail.com>
2026-01-06 09:26:19 -0700

    nix configuration changes

--- flake.nix
+++ flake.nix
@@ -10,12 +10,45 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      nativeLibs = [
+        pkgs.gmp
+        pkgs.mesa
+        pkgs.mesa_glu
+        pkgs.freeglut
+        pkgs.xorg.libX11
+      ];
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          ghc-wasm.packages.${system}.default
-          ghc-wasm.packages.${system}.cabal
-        ];
+      devShells = {
+        ${system} = {
+          native = pkgs.mkShell {
+            packages =
+              [
+                pkgs.stack
+                pkgs.cabal-install
+                pkgs.hpack
+                pkgs.ghcid
+                pkgs.haskell-language-server
+                pkgs.pkg-config
+              ]
+              ++ nativeLibs;
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath nativeLibs}:$LD_LIBRARY_PATH"
+            '';
+          };
+
+          wasm = pkgs.mkShell {
+            packages = [
+              ghc-wasm.packages.${system}.default
+              ghc-wasm.packages.${system}.cabal
+              pkgs.cabal-install
+              pkgs.hpack
+              pkgs.nodejs
+              pkgs.pnpm
+            ];
+          };
+
+          default = self.devShells.${system}.native;
+        };
       };
     };
 }
